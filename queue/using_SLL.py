f1ec1cf629b786efbaee1c0c5827400338e2a3fe
iamrstar <rajchatterji20@gmail.com>
2025-07-06 20:35:05 -0700

    quue using sll

---
+++ queue/using_SLL.py
@@ -0,0 +1,12 @@
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None   
+        self.item_count = 0
+    def is_empty(self):
+        return self.front==None
+    def enqueue(self,data):
+        
+     
+
+
