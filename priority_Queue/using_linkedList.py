9b483c189fd83935fa8d52d55056480e1f358a3d
iamrstar <rajchatterji20@gmail.com>
2025-07-14 21:26:24 +0530

    priority queue using linkrd list

--- priority_Queue/using_linkedList.py
+++ priority_Queue/using_linkedList.py
@@ -12,8 +12,32 @@ class PriorityQueue:
         return self.item_count == 0
     def push(self,data,priority):
         n = Node(data,priority)
-        if self.is_empty():
+        if not self.start or priority <self.start.priority:
+            n.next = self.start
             self.start = n
         else:
+            temp = self.start
+            while temp.next and temp.next.priority <= priority:
+                temp = temp.next
+            n.next = temp.next
+            temp.next = n
+        self.item_count += 1
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        else:
+             data = self.start.item
+             self.start = self.start.next
+             self.item_count -= 1
+             return data       
+    def size(self):
+        return self.item_count
+p = PriorityQueue()
+p.push("A",2)
+p.push("B",1)
+p.push("C",3)
+print(p.pop())
+print(p.size()) 
+       
             
          
