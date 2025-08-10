cedd52ecdf9e710e86f6dbd5cbecdbf6e1dfaa3b
iamrstar <rajchatterji20@gmail.com>
2025-08-10 09:53:06 +0530

    some questions

---
+++ Leetcode Questions/TwoSum.py
@@ -0,0 +1,10 @@
+ def twoSum(self, nums, target):
+      
+        num_map = {}
+        for i, num in enumerate(nums):
+            comp = target - num
+            if comp in num_map:
+                return [num_map[comp], i]
+            num_map[num] = i
+      
+        return []
